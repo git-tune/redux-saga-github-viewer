a24ec703c25088937095f77ae7f1256c8eb40390
tune <hisatsune01@gmail.com>
2020-07-22 14:44:36 +0900

    add components > templates

---
+++ Documents/GitHub/redux-saga-github-viewer/src/components/templates.js/PullRequest.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  text-align: center;
+  margin-top: 180px;
+  font-size: 18px;
+  white-space: nowrap;
+`;
+
+const PullRequest = () => {
+  return (
+    <Container>
+      <h1>Pull Request</h1>
+    </Container>
+  );
+};
+
+export default PullRequest;
