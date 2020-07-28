c7d81620b217adb411e5bbdbcd6ac197f82242a5
tune <hisatsune01@gmail.com>
2020-07-28 21:12:42 +0900

    Updates

---
+++ precache-manifest.d51331a6828d53827a015d320743e04d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "543e0c4ea948485c9a90331b5054a7f4",
+    "url": "/redux-saga-github-viewer/index.html"
+  },
+  {
+    "revision": "18fc942f3c1cbb1dfcd6",
+    "url": "/redux-saga-github-viewer/static/js/2.1eb540a9.chunk.js"
+  },
+  {
+    "revision": "cb4293a028d0175415ddb31dc1aaaefb",
+    "url": "/redux-saga-github-viewer/static/js/2.1eb540a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "859ad5737c74f213ab18",
+    "url": "/redux-saga-github-viewer/static/js/main.d018fdb5.chunk.js"
+  },
+  {
+    "revision": "0b6e148636e00d0b7f32",
+    "url": "/redux-saga-github-viewer/static/js/runtime-main.8038e559.js"
+  }
+]);
